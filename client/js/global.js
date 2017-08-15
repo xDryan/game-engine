720b5c0943912a918eb63d0d92c9afe7e9d5e7b7
xDryan <bryan.truwant@gmail.com>
2017-08-15 16:59:47 +0200

    Modification du fichier global.js en proprietes.js et suppression du fichier moteur.js

--- client/js/global.js
+++
@@ -1,5 +0,0 @@
-var texturePath = 'textures/grass.png'; // Le chemin de la texture que l'on peut placer actuellement
-var tailleTextures = 128; // La taille des texutres du jeu
-var nbLignes = 3; // Le nombre de lignes de la map
-var nbColonnes = 3; // Le nombre de colonnes de la map
-var adresseServeur = "http://localhost/game-engine/serveur"; // Adresse du dossier sur le serveur
